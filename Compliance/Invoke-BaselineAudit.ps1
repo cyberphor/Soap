35e38d59417a1b6a43c04d6f154a172d34178dc2
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2021-05-24 23:07:41 -0600

    Update Invoke-BaselineAudit.ps1

--- Compliance/Invoke-BaselineAudit.ps1
+++ Compliance/Invoke-BaselineAudit.ps1
@@ -3,4 +3,7 @@ $SearchBase = ""
 $Computers = Get-AdComputer -Filter * -SearchBase $SearchBase | Select-Object -ExpandProperty Name
 
 Invoke-Command -ComputerName $Computers -ErrorAction Ignore -FilePath ./Get-WirelessComputers.ps1 |
-Select Hostname, Username, Model, SerialNumber
+Select Hostname, Username, Model, SerialNumber | Format-Table -AutoSize
+
+Invoke-Command -ComputerName $Computers -ErrorAction Ignore -FilePath ./Get-WirelessComputers.ps1 |
+Select Hostname, Username, Model, SerialNumber, Administrator | Format-Table -AutoSize
