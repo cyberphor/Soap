0c245233f4fcf974d5c9c9421095c914baa98b67
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2021-05-25 02:59:11 -0600

    Update Invoke-CyberEffect.ps1

--- AdversaryEmulation/Invoke-CyberEffect.ps1
+++ AdversaryEmulation/Invoke-CyberEffect.ps1
@@ -1,3 +1,10 @@
+$Credential = Get-Credential
 $Computers = ""
+
+$Computers | 
+foreach {
+    $Session = New-PSSession -ComputerName $_ -Credential $Credential -ErrorAction Ignore
+    Copy-Item .\Start-Scare.ps1 -Destination "C:\" -ToSession $Session -ErrorAction Ignore
+}
 
 Invoke-Command -ComputerName $Computers -ErrorAction Ignore -FilePath ./Start-RollingReboot.ps1
