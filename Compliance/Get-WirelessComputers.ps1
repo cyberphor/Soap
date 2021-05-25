befdd9ccedd07c597345fc23fb4ecde3a12fb2fb
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2021-05-24 22:31:57 -0600

    Create Get-WirelessComputers.ps1

---
+++ Compliance/Get-WirelessComputers.ps1
@@ -0,0 +1,23 @@
+function Get-WirelessComputers {
+  $Hostname = Get-WmiObject -Class Win32_NetworkAdapter |
+    Where-Object { $_.Name -like "*Wireless*" } |
+    Select-Object -ExpandProperty PsComputerName -First 1
+    
+  if ($Hostname) {
+    $Username = (Get-WmiObject -Class Win32_NetworkLoginProfile | 
+      Where-Object { $_.Name -notlike "*admin*" -and $_.Name -notlike "*service*" } |
+      Sort-Object -Property LastLogon -Descending |
+      Select-Object -First 1 -ExpandProperty Name).Split("\")[1]
+    $Model = Get-WmiObject -Class Win32_ComputerSystem | Select-Object -ExpandProperty Model
+    $SerialNumber = Get-WmiObject -Class Win32_BIOS | Select-Object -ExpandProperty SerialNumber
+    
+    $ComputerInformation = New-Object psobject
+    Add-Member -InputObject $ComputerInformation -MemberType NoteProperty -Name Hostname -Value $Hostname
+    Add-Member -InputObject $ComputerInformation -MemberType NoteProperty -Name Username -Value $Username
+    Add-Member -InputObject $ComputerInformation -MemberType NoteProperty -Name Model -Value $Model
+    Add-Member -InputObject $ComputerInformation -MemberType NoteProperty -Name SerialNumber -Value $SerialNumber
+    $ComputerInformation
+  }
+}
+
+Get-WirelessComputers
