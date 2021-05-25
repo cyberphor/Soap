06ad3c22d7da1bc920d19fdf3c54c17fe339fd45
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2021-05-24 23:06:52 -0600

    Create Get-NewLocalAdmins.ps1

---
+++ Compliance/Get-NewLocalAdmins.ps1
@@ -0,0 +1,29 @@
+$BaselineAdmins = "Administrator","elliot.alderson"
+
+function Get-NewLocalAdmins {
+  $Administrator = Get-LocalGroupMember -Group "Administrators" |
+    Where-Object { $_.ObjectClass -notmatch "Group" } |
+    Select-Object -ExpandProperty Name |
+    ForEach-Object { $_.Split("\")[1] } |
+    Where-Object { $BaselineAdmins -notcontains $_ }
+    
+  if ($Administrator) {
+    if ($Administrator.Count -gt 1) { $Administrator = $Administrator[0] }
+    $Username = (Get-WmiObject -Class Win32_NetworkLoginProfile | 
+      Where-Object { $_.Name -notlike "*admin*" -and $_.Name -notlike "*service*" } |
+      Sort-Object -Property LastLogon -Descending |
+      Select-Object -First 1 -ExpandProperty Name).Split("\")[1]
+    $Model = Get-WmiObject -Class Win32_ComputerSystem | Select-Object -ExpandProperty Model
+    $SerialNumber = Get-WmiObject -Class Win32_BIOS | Select-Object -ExpandProperty SerialNumber
+    
+    $ComputerInformation = New-Object psobject
+    Add-Member -InputObject $ComputerInformation -MemberType NoteProperty -Name Hostname -Value $Hostname
+    Add-Member -InputObject $ComputerInformation -MemberType NoteProperty -Name Username -Value $Username
+    Add-Member -InputObject $ComputerInformation -MemberType NoteProperty -Name Model -Value $Model
+    Add-Member -InputObject $ComputerInformation -MemberType NoteProperty -Name SerialNumber -Value $SerialNumber
+    Add-Member -InputObject $ComputerInformation -MemberType NoteProperty -Name Administrator -Value $Administrator
+    $ComputerInformation
+  }
+}
+
+Get-NetLocalAdmins
