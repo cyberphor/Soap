49100840d1c258c1f853335439c7e1dcbfa36705
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2021-05-25 04:50:19 -0600

    Update Invoke-BaselineAudit.ps1

--- Compliance/Invoke-BaselineAudit.ps1
+++ Compliance/Invoke-BaselineAudit.ps1
@@ -2,8 +2,15 @@ $SearchBaseDefault = (Get-AdDomain).DistinguishedName
 $SearchBase = ""
 $Computers = Get-AdComputer -Filter * -SearchBase $SearchBase | Select-Object -ExpandProperty Name
 
-Invoke-Command -ComputerName $Computers -ErrorAction Ignore -FilePath ./Get-WirelessComputers.ps1 |
-Select Hostname, Username, Model, SerialNumber | Format-Table -AutoSize
 
 Invoke-Command -ComputerName $Computers -ErrorAction Ignore -FilePath ./Get-NewLocalAdmins.ps1 |
 Select Hostname, Username, Model, SerialNumber, Administrator | Format-Table -AutoSize
+
+Invoke-Command -ComputerName $Computers -ErrorAction Ignore -FilePath ./Get-OpenPorts.ps1 |
+Select Hostname, Username, Model, SerialNumber, OpenPorts | Format-Table -AutoSize
+
+Invoke-Command -ComputerName $Computers -ErrorAction Ignore -FilePath ./Get-Shares.ps1 |
+Select Hostname, Username, Model, SerialNumber, Shares | Format-Table -AutoSize
+
+Invoke-Command -ComputerName $Computers -ErrorAction Ignore -FilePath ./Get-WirelessComputers.ps1 |
+Select Hostname, Username, Model, SerialNumber | Format-Table -AutoSize
