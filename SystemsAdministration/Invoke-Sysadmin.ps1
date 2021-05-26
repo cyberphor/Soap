7efeb6b64c420e3767ec99758809c70ea37e2962
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2021-05-26 16:55:37 -0600

    Create Invoke-Sysadmin.ps1

---
+++ SystemsAdministration/Invoke-Sysadmin.ps1
@@ -0,0 +1,19 @@
+Param(
+    [switch]$AssetInventory,
+    [switch]$DiskSpace,
+    [string]$SearchBase = (Get-AdDomain).DistinguishedName
+)
+
+if ($AssetInventory) {
+    $Script = ".\Get-Asset.ps1"
+    $Output = "Hostname", "Username", "Model", "SerialNumber", "FilePath", "Sha256Hash"
+} elseif ($DiskSpace) {
+    $Script = ".\Get-DiskSpace"
+    $Output = "Hostname", "Username", "Model", "SerialNumber"
+} else {
+    exit
+}
+
+$Computers = Get-AdComputer -Filter * -SearchBase $SearchBase | Select-Object -ExpandProperty Name
+Invoke-Command -ComputerName $Computers -ErrorAction Ignore -FilePath $Script |
+Select $Output
