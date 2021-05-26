99ee156ce275608680061c80e6b38979f3db4ee2
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2021-05-25 21:40:05 -0600

    Update Get-Privileges.ps1

--- Compliance/Get-Privileges.ps1
+++ Compliance/Get-Privileges.ps1
@@ -1,5 +1,27 @@
 function Get-Privileges {
-  SecEdit.exe /export /areas USER_RIGHTS /cfg C:\Users\Public\UserRights.txt
+    SecEdit.exe /export /areas USER_RIGHTS /cfg ./user-rights.txt /quiet
+    $Privileges = Get-Content .\user-rights.txt | Where-Object { $_.StartsWith("Se") }
+    Remove-Item .\user-rights.txt | Out-Null
+    $Privileges |
+    ForEach-Object {
+        $Assignment = $_.Split(" = ")
+        $Privilege = $Assignment[0]
+        $Sids = $Assignment[3].Split(",") |
+            ForEach-Object {
+                if ($_.StartsWith("*")) {
+                    $_.Substring(1)
+                } else {
+                    $_
+                }
+            }
+        $Sids | 
+        ForEach-Object {
+            $Output = New-Object psobject
+            Add-Member -InputObject $Output -MemberType NoteProperty -Name Privilege -Value $Privilege
+            Add-Member -InputObject $Output -MemberType NoteProperty -Name Sid -Value $_
+            $Output
+        }
+    }
 }
 
 Get-Privileges
