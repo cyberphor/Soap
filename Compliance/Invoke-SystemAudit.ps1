e39b48ad4a6bb9a0cac445339e916ee246f54804
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2021-05-26 21:10:26 -0600

    Update Invoke-SystemAudit.ps1

--- Compliance/Invoke-SystemAudit.ps1
+++ Compliance/Invoke-SystemAudit.ps1
@@ -1,16 +1,27 @@
-$SearchBaseDefault = (Get-AdDomain).DistinguishedName
-$SearchBase = ""
+Param(
+    [switch]$LocalAdmins,
+    [switch]$OpenPorts,
+    [switch]$Shares,
+    [switch]$WirelessComputers,
+    [string]$SearchBase = (Get-AdDomain).DistinguishedName
+)
+
+if ($LocalAdmins) {
+    $Script = ".\Get-LocalAdmins.ps1"
+    $Output = "Hostname", "Username", "Model", "SerialNumber", "Administrator"
+} elseif ($OpenPorts) {
+    $Script = ".\Get-OpenPorts.ps1"
+    $Output = "Hostname", "Username", "Model", "SerialNumber","OpenPorts"
+} elseif ($Shares) {
+    $Script = ".\Get-Shares.ps1"
+    $Output = "Hostname", "Username", "Model", "SerialNumber","Shares"
+} elseif ($WirelessComputers) {
+    $Script = ".\Get-WirelessComputers.ps1"
+    $Output = "Hostname", "Username", "Model", "SerialNumber"
+} else {
+    exit
+}
+
 $Computers = Get-AdComputer -Filter * -SearchBase $SearchBase | Select-Object -ExpandProperty Name
-
-
-Invoke-Command -ComputerName $Computers -ErrorAction Ignore -FilePath ./Get-NewLocalAdmins.ps1 |
-Select Hostname, Username, Model, SerialNumber, Administrator | Format-Table -AutoSize
-
-Invoke-Command -ComputerName $Computers -ErrorAction Ignore -FilePath ./Get-OpenPorts.ps1 |
-Select Hostname, Username, Model, SerialNumber, OpenPorts | Format-Table -AutoSize
-
-Invoke-Command -ComputerName $Computers -ErrorAction Ignore -FilePath ./Get-Shares.ps1 |
-Select Hostname, Username, Model, SerialNumber, Shares | Format-Table -AutoSize
-
-Invoke-Command -ComputerName $Computers -ErrorAction Ignore -FilePath ./Get-WirelessComputers.ps1 |
-Select Hostname, Username, Model, SerialNumber | Format-Table -AutoSize
+Invoke-Command -ComputerName $Computers -ErrorAction Ignore -FilePath $Script |
+Select $Output | Format-Table -AutoSize
