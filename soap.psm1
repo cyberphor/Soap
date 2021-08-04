103f0662d202e6b86927e9d09e5ab899e8daaa39
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2021-08-04 17:37:05 -0600

    Update soap.psm1

--- soap.psm1
+++ soap.psm1
@@ -2,6 +2,40 @@ function Get-Computers {
   Get-AdComputer -Filter * | Select-Object -ExpandProperty Name
 }
 
+function Get-ComputersOnline {
+    $NetworkId = "10.11.12"
+    $NetworkRange = 1..254
+    Get-Event -SourceIdentifier "Ping-*" | Remove-Event -ErrorAction Ignore
+    Get-EventSubscriber -SourceIdentifier "Ping-*" | Unregister-Event -ErrorAction Ignore
+
+    $NetworkRange |
+    ForEach-Object {
+        $IpAddress = $NetworkId + $_
+        $Event = "Ping-" + $IpAddress
+        New-Variable -Name $Event -Value (New-Object System.Net.NetworkInformation.Ping)
+        Register-ObjectEvent -InputObject (Get-Variable $Event -ValueOnly) -EventName PingCompleted -SourceIdentifier $Event
+        (Get-Variable $Event -ValueOnly).SendAsync($IpAddress,2000,$Event)
+    }
+
+    while ($Pending -lt $NetworkRange.Count) {
+        Wait-Event -SourceIdentifier "Ping-*" | Out-Null
+        Start-Sleep -Milliseconds 10
+        $Pending = (Get-Event -SourceIdentifier "Ping-*").Count
+    }
+
+    $ComputersOnline = @()
+    Get-Event -SourceIdentifier "Ping-*" |
+    ForEach-Object {
+        if ($_.SourceEventArgs.Reply.Status -eq "Success") {
+            $ComputersOnline += $_.SourceEventArgs.Reply.Address.IpAddressToString
+            Remove-Event $_.SourceIdentifier
+            Unregister-Event $_.SourceIdentifier
+        }
+    }
+
+    $ComputersOnline | Sort-Object { $_ -as [Version] }
+}
+
 function Get-LocalGroupAdministrators {
   $Computers = Get-Computers
   Invoke-Command -ComputerName $Computers -ScriptBlock {
