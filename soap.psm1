592549e54be030152339618119966560cdcf899a
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2021-08-20 23:46:01 -0600

    Update soap.psm1

--- soap.psm1
+++ soap.psm1
@@ -1,3 +1,23 @@
+function Import-SoapModule {
+    $Owner = "cyberphor"
+    $Repo = "soap"
+    $RepoFolder = "$Repo\$Repo-main\"
+    $RepoContents = $RepoFolder + "*"
+    $Download = $Repo + ".zip"
+    $ModulesFolder = "C:\Program Files\WindowsPowerShell\Modules\"
+    $Uri = "https://github.com/$Owner/$Repo/archive/refs/heads/main.zip"
+    $SoapModule = $ModulesFolder + $Repo + "\$Repo.psm1"
+    if (-not (Test-Path -Path $SoapModule)) {
+        Invoke-WebRequest -Uri $Uri -OutFile $Download
+        Expand-Archive -Path $Download -DestinationPath $Repo
+        Move-Item -Path $RepoContents -Destination $Repo
+        Remove-Item $Download 
+        Remove-Item $RepoFolder
+        Move-Item -Path $Repo -Destination $ModulesFolder
+    }
+    Import-Module $Repo -Force
+}
+
 function Read-SoapModule {
     ise "C:\Program Files\WindowsPowerShell\Modules\soap\soap.psm1"
 }
