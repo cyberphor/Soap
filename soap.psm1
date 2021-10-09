7019450dda2d33e36f87f5c8f2920a1fcdbcc70b
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2021-10-09 17:26:43 -0600

    Update soap.psm1

--- soap.psm1
+++ soap.psm1
@@ -329,3 +329,29 @@ function New-IncidentResponse {
             | time resolved      | 2021-08-29 0900      | vf       | 2021-08-29 0900  | 
      #>
  }
+
+function Import-AdUsersFromCsv {
+    $Password = ConvertTo-SecureString -String '1qaz2wsx!QAZ@WSX' -AsPlainText -Force
+
+    Import-Csv -Path .\users.csv |
+    ForEach-Object {
+        $Name = $_.LastName + ', ' + $_.FirstName
+        $SamAccountName = ($_.FirstName + '.' + $_.LastName).ToLower()
+        $UserPrincipalName = $SamAccountName + '@evilcorp.local'
+        $Description = $_.Description
+        $ExpirationDate = Get-Date -Date 'October 31 2022'
+        New-AdUser `
+            -Name $Name `
+            -DisplayName $Name `
+            -GivenName $_.FirstName `
+            -Surname $_.LastName `
+            -SamAccountName $SamAccountName `
+            -UserPrincipalName $UserPrincipalName `
+            -Description $Description `
+            -ChangePasswordAtLogon $true `
+            -AccountExpirationDate $ExpirationDate `
+            -Enabled $true `
+            -Path 'OU=Users,OU=evilcorp,DC=local' `
+            -AccountPassword $Password
+    }
+}
