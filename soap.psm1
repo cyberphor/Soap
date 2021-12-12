0f364b4e89a2ab3c8822690d9934322393a6db90
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2021-12-12 15:51:32 -0700

    Update soap.psm1

--- soap.psm1
+++ soap.psm1
@@ -129,7 +129,7 @@ function Invoke-WinEventParser {
     param(
         [Parameter(Position=0)][string]$ComputerName,
         [ValidateSet("Application","Security","System","ForwardedEvents","Microsoft-Windows-PowerShell/Operational")][Parameter(Position=1)][string]$LogName,
-        [ValidateSet("4104","4624","4625","4672","4688","5156","6416")][Parameter(Position=2)]$EventId,
+        [ValidateSet("4104","4624","4625","4663","4672","4688","4697","5140","5156","6416")][Parameter(Position=2)]$EventId,
         [Parameter(Position=3)][int]$DaysAgo=1,
         [Parameter(Position=4)][switch]$TurnOffOutputFilter
     )
@@ -160,10 +160,14 @@ function Invoke-WinEventParser {
         if ($EventId -eq "4104") { $Properties = "TimeCreated","SecurityUserId","ScriptBlockText" }
         elseif ($EventId -eq "4624") { $Properties = "TimeCreated","IpAddress","TargetUserName","LogonType" }
         elseif ($EventId -eq "4625") { $Properties = "TimeCreated","IpAddress","TargetUserName","LogonType" }
+        elseif ($EventId -eq "4663") { $Properties = "*" }
         elseif ($EventId -eq "4672") { $Properties = "TimeCreated","SubjectUserSid","SubjectUserName" }
         elseif ($EventId -eq "4688") { $Properties = "TimeCreated","TargetUserName","NewProcessName","CommandLine" }
-        elseif ($EventId -eq "5156") { $Properties = "TimeCreated","TargetUserName","LogonType" }
+        elseif ($EventId -eq "4697") { $Properties = "*" }
+        elseif ($EventId -eq "5140") { $Properties = "*" }
+        elseif ($EventId -eq "5156") { $Properties = "TimeCreated","SourceAddress","DestAddress","DestPort" }
         elseif ($EventId -eq "6416") { $Properties = "TimeCreated","SubjectUserName","ClassName","DeviceDescription" }
+        else { $Properties = "*" }
         Get-WinEvent -FilterHashtable @{ LogName=$LogName; Id=$EventId } | 
         Read-WinEvent | 
         Select-Object -Property $Properties
