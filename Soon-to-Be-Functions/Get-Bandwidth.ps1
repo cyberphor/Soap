7ff52417fa3f1d3e18a84f8ff260501360fb5642
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2021-12-12 18:20:23 -0700

    Delete Get-Bandwidth.ps1

--- Soon-to-Be-Functions/Get-Bandwidth.ps1
+++
@@ -1,8 +0,0 @@
-While ($true) {
-    $Speed = Test-Connection -Count 1 -ComputerName 8.8.8.8 | 
-    Select-Object -ExpandProperty ResponseTime
-    
-    Clear-Host
-    Write-Host "`n[+] Bandwidth (ms): $($Speed)"
-    Start-Sleep -Seconds 1
-}
