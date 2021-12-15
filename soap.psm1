2d45927c14481b12a62cfb4600f780563594f831
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2021-12-15 06:33:03 -0700

    Re-wrote a function.

--- soap.psm1
+++ soap.psm1
@@ -107,6 +107,16 @@ function Get-Permissions {
     }
 }
 
+function Get-Shares {
+    param(
+        [string[]]$Whitelist = @("ADMIN$","C$","IPC$")
+    )
+
+    Get-SmbShare | 
+    Where-Object { $Whitelist -notcontains $_.Name } |
+    Select-Object -Property Name, Path, Description
+}
+
 function Get-TcpPorts {
     Get-NetTCPConnection | 
     Select-Object @{ "Name" = "ProcessId"; "Expression" = { $_.OwningProcess }},LocalPort,@{ "Name" = "ProcessName"; "Expression" = { (Get-Process -Id $_.OwningProcess).Name }},RemoteAddress |
