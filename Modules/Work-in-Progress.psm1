4f24fa32a5c98301932c1f86589928d3249a8f65
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2021-12-20 19:27:49 -0700

    Update Work-in-Progress.psm1

--- Modules/Work-in-Progress.psm1
+++ Modules/Work-in-Progress.psm1
@@ -1,1 +1,11 @@
-﻿
+function Get-EventForwarders {
+    param(
+      [Parameter(Mandatory)][string]$ComputerName,
+      [Parameter(Mandatory)][string]$Subscription = "Forwarded Events"
+    )
+    Invoke-Command -ComputerName $ComputerName -ScriptBlock {
+        $Key = "HKLM:\Software\Microsoft\Windows\CurrentVersion\EventCollector\Subscriptions\$Subscription\EventSources"
+        $EventForwarders = (Get-ChildItem $Key).Name | ForEach-Object { $_.Split("\")[9] }
+        return $EventForwarders
+    }
+}
