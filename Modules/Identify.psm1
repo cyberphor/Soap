bf90fe556d83fc88b55bb2b598683b023da04c4d
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2021-12-20 18:08:11 -0700

    Update Identify.psm1

--- Modules/Identify.psm1
+++ Modules/Identify.psm1
@@ -1,3 +1,20 @@
+function Get-DiskSpace {
+    Get-CimInstance -Class Win32_LogicalDisk |
+    Select-Object -Property @{
+        Label = 'DriveLetter'
+        Expression = { $_.Name }
+    },@{
+        Label = 'FreeSpace (GB)'
+        Expression = { ($_.FreeSpace / 1GB).ToString('F2') }
+    },@{
+        Label = 'TotalSpace (GB)'
+        Expression = { ($_.Size / 1GB).ToString('F2') }
+    },@{
+        Label = 'SerialNumber'
+        Expression = { $_.VolumeSerialNumber }
+    }
+}
+
 function Get-IpAddressRange {
     param([Parameter(Mandatory)][string[]]$Network)
     $IpAddressRange = @()
