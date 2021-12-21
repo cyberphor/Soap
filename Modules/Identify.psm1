5d37d0acfdb928d7d813efead5ff6053cdb45ab5
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2021-12-21 07:03:38 -0700

    Added a function.

--- Modules/Identify.psm1
+++ Modules/Identify.psm1
@@ -15,6 +15,23 @@ function Get-DiskSpace {
     }
 }
 
+function Get-EnterpriseVisbility {
+    param(
+        [Parameter(Mandatory)][string]$Network,
+        [Parameter(Mandatory)][string]$EventCollector
+    )
+    $ActiveIps = Get-IpAddressRange -Network $Network | Test-Connections
+    $AdObjects = (Get-AdComputer -Filter "*").Name
+    $EventForwarders = Get-EventForwarders -ComputerName $EventCollector
+    $WinRmclients = Get-WinRmClients
+    $Visbility = New-Object -TypeName psobject
+    $Visbility | Add-Member -MemberType NoteProperty -Name ActiveIps -Value $ActiveIps.Count
+    $Visbility | Add-Member -MemberType NoteProperty -Name AdObjects -Value $AdObjects.Count
+    $Visbility | Add-Member -MemberType NoteProperty -Name EventForwarders -Value $EventForwarders.Count
+    $Visbility | Add-Member -MemberType NoteProperty -Name WinRmClients -Value $WinRmclients.Count
+    return $Visbility
+}
+
 function Get-EventForwarders {
     param(
       [string]$ComputerName,
