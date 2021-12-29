fca9c784da1c4aec4b7bc3644875d45d95c8edb1
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2021-12-28 21:45:40 -0700

    Update Work-in-Progress.psm1

--- Modules/Work-in-Progress.psm1
+++ Modules/Work-in-Progress.psm1
@@ -1,3 +1,18 @@
+function ConvertFrom-CsvToMarkdownTable {
+    <# .EXAMPLE 
+    ConvertFrom-CsvToMarkdownTable -Path .\CyberFunctions.csv
+    #>
+    param([Parameter(Mandatory)][string]$Path)
+    if (Test-Path -Path $Path) {
+        $Csv = Get-Content $Path
+        $Headers = $Csv | Select-Object -First 1
+        $NumberOfHeaders = ($Headers.ToCharArray() | Where-Object { $_ -eq ',' }).Count + 1
+        $MarkdownTable = $Csv | ForEach-Object { '| ' + $_.Replace(',',' | ') + ' |' }
+        $MarkdownTable[0] += "`r`n" + ('| --- ' * $NumberOfHeaders) + '|'
+        return $MarkdownTable 
+    }
+}
+
 filter Read-WinEvent {
     $XmlData = [xml]$_.ToXml()
     $Event = New-Object -TypeName PSObject
