a43c4dd82a6a812f4890a2e12aaaee26628e4934
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2022-02-06 19:44:15 -0700

    Update Microsoft.PowerShell_profile.ps1

--- Profiles/Microsoft.PowerShell_profile.ps1
+++ Profiles/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,4 @@
-﻿Import-Module PSCyberTools
+Import-Module PSCyberTools
+$host.ui.rawui.WindowTitle = "Terminal"
+function prompt { 'PS ' + ($pwd -split '\\')[0]+' '+$(($pwd -split '\\')[-1] -join '\') + '> ' }
 Set-Location -Path "C:\Users\$env:USERNAME\Desktop"
