8a2a1b7104563d32839db3f13b0fa265ec9b965f
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2022-03-22 11:03:34 -0600

    Made some edits.

--- soap.psm1
+++ soap.psm1
@@ -527,8 +527,9 @@ function Remove-CustomModule {
 }
 
 function Send-Alert {
+    [CmdletBinding(DefaultParameterSetName = 'Log')]
     Param(
-        [Parameter(Mandatory, Position = 0)][ValidateSet("Log","Email")][string]$AlertMethod,
+        [Parameter(Mandatory, Position = 0)][ValidateSet("Balloon","Log","Email")][string]$AlertMethod,
         [Parameter(Mandatory, Position = 1)]$Subject,
         [Parameter(Mandatory, Position = 2)]$Body,
         [Parameter(ParameterSetName = "Log")][string]$LogName,
@@ -541,7 +542,37 @@ function Send-Alert {
         [Parameter(ParameterSetName = "Email")][string]$EmailPassword,
         [Parameter(ParameterSetName = "Email")][string]$EmailAddressDestination
     )
-    if ($AlertMethod -eq "Log") {
+    <#
+        .SYNOPSIS
+        Sends an alert. 
+
+        .DESCRIPTION
+        When called, this function will either write to the Windows Event log, send an email, or generate a Windows balloon tip notification.
+        
+        .LINK
+        https://mcpmag.com/articles/2017/09/07/creating-a-balloon-tip-notification-using-powershell.aspx
+    #>
+
+    if ($AlertMethod -eq "Balloon") {
+        Add-Type -AssemblyName System.Windows.Forms
+        Unregister-Event -SourceIdentifier IconClicked -ErrorAction Ignore
+        Remove-Job -Name IconClicked -ErrorAction Ignore
+        Remove-Variable -Name Balloon -ErrorAction Ignore
+        $Balloon = New-Object System.Windows.Forms.NotifyIcon
+        [void](Register-ObjectEvent `
+            -InputObject $Balloon `
+            -EventName MouseDoubleClick `
+            -SourceIdentifier IconClicked `
+            -Action { $Balloon.Dispose() }
+        )
+        $IconPath = (Get-Process -Id $pid).Path
+        $Balloon.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($IconPath)
+        $Balloon.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Warning
+        $Balloon.BalloonTipTitle = $Subject
+        $Balloon.BalloonTipText = $Body
+        $Balloon.Visible = $true
+        $Balloon.ShowBalloonTip(10000)
+    } elseif ($AlertMethod -eq "Log") {
         $LogExists = Get-EventLog -LogName $LogName -Source $LogSource -ErrorAction Ignore -Newest 1
         if (-not $LogExists) {
             New-EventLog -LogName $LogName -Source $LogSource -ErrorAction Ignore
