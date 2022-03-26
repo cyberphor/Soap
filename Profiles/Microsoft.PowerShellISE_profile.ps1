87ac385845e1b68ecc13bba6b53780489c150fbb
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2022-03-25 21:21:52 -0600

    Removed some files.

--- Profiles/Microsoft.PowerShellISE_profile.ps1
+++
@@ -1,2 +0,0 @@
-Import-Module PSCyberTools
-Set-Location -Path "C:\Users\$env:USERNAME\Desktop"
--- Profiles/Microsoft.PowerShell_profile.ps1
+++
@@ -1,4 +0,0 @@
-Import-Module PSCyberTools
-$host.ui.rawui.WindowTitle = "Terminal"
-function prompt { 'PS ' + ($pwd -split '\\')[0]+' '+$(($pwd -split '\\')[-1] -join '\') + '> ' }
-Set-Location -Path "C:\Users\$env:USERNAME\Desktop"
