347fd25c984f1507c825a4d0dc62da6fda2ba732
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2022-04-09 19:10:10 -0700

    Update soap.psm1

--- soap.psm1
+++ soap.psm1
@@ -77,8 +77,10 @@ function Copy-FileToRemotePublicFolder {
 		}
 	}
 }
-function Edit-PowerShellModule {
-    param([string]$Name)
+function Edit-Module {
+    Param(
+        [Parameter(Mandatory)][string]$Name
+    )
     $Module = "C:\Program Files\WindowsPowerShell\Modules\$Name\$Name.psm1"
     $Expression = 'powershell_ise.exe "$Module"'
     if (Test-Path -Path $Module) {
@@ -440,7 +442,7 @@ function Get-LocalAdministrators {
     }
 }
 
-function Get-PowerShellModuleFunctions {
+function Get-ModuleFunctions {
     param([string]$Module)
     (Get-Module $Module | Select-Object -Property ExportedCommands).ExportedCommands.Keys 
 }
@@ -823,7 +825,7 @@ function Remove-App {
     }
 }
 
-function Remove-PowerShellModule {
+function Remove-Module {
     param([Parameter(Mandatory)][string]$Name)
     $Module = "C:\Program Files\WindowsPowerShell\Modules\$Name"
     if (Test-Path -Path $Module) {
@@ -981,6 +983,42 @@ function Import-AdUsersFromCsv {
             -Enabled $true `
             -Path 'OU=Users,OU=evilcorp,DC=local' `
             -AccountPassword $Password
+    }
+}
+
+function Set-FirewallPolicy {
+    Param(
+        [string[]]$AuthorizedProtocol = "ICMP",
+        [int[]]$AuthorizedPorts = @(53,80,443,5985),
+        [int[]]$RemoteManagementPorts = @(5985),
+        [ipaddress]$ManagementIpAddress
+    )
+
+    Write-Output "Configuring DoD Windows 10 STIG Requirement V-220725 (Inbound exceptions to the firewall on Windows 10 domain workstations must only allow authorized remote management hosts)."
+    
+    # enable Windows Remote Management
+    Enable-PSRemoting -Force
+    if ($ManagementIpAddress) {
+        Set-Item -Path WSMan:\localhost\Service\ -Name IPv4Filter -Value $ManagementIpAddress
+    }
+
+    # disable all rules allowing inbound connections (except for Windows Remote Management)
+    Get-NetFirewallRule -Direction Inbound -Action Allow |
+    ForEach-Object { 
+        $NotAuthorizedPort = $RemoteManagementPorts -notcontains $($_ | Get-NetFirewallPortFilter).RemotePort
+        if ($NotAuthorizedPort) {
+            $_ | Set-NetFirewallRule -Enabled False
+        }
+    }
+
+    # disable all rules allowing outbound connections except for those authorized
+    Get-NetFirewallRule -Direction Outbound -Action Allow | 
+    ForEach-Object { 
+        $NotAuthorizedProtocol = $AuthorizedProtocols -notcontains $($_ | Get-NetFirewallPortFilter).Protocol
+        $NotAuthorizedPort = $AuthorizedPorts -notcontains $($_ | Get-NetFirewallPortFilter).RemotePort
+        if ($NotAuthorizedProtocol -or $NotAuthorizedPort) {
+            $_ | Set-NetFirewallRule -Enabled False
+        }
     }
 }
 
