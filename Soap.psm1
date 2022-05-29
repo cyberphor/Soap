2e1c1f488058b62118c2d8ff203f86b665e14950
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2022-05-28 18:14:58 -0700

    Update Soap.psm1

--- Soap.psm1
+++ Soap.psm1
@@ -1942,15 +1942,11 @@ function Start-RollingReboot {
         [int]$Duration = 60
     )
     $TaskName = "Rolling Reboot"
-    $Action= New-ScheduledTaskAction -Execute "shutdown.exe" -Argument "/r /t 0" 
-    $Trigger= New-ScheduledTaskTrigger -At $(Get-Date) -Once -RepetitionInterval $(New-TimeSpan -Minutes $Interval) -RepetitionDuration $(New-TimeSpan -Minutes $Duration)
-    $User= "NT AUTHORITY\SYSTEM" 
+    $Action = New-ScheduledTaskAction -Execute "shutdown.exe" -Argument "/r /t 0" 
+    $Trigger = New-ScheduledTaskTrigger -At $(Get-Date) -Once -RepetitionInterval $(New-TimeSpan -Minutes $Interval) -RepetitionDuration $(New-TimeSpan -Minutes $Duration)
+    $User = 'NT AUTHORITY\SYSTEM' 
     Register-ScheduledTask -TaskName $TaskName -Action $Action -Trigger $Trigger -User $User -RunLevel Highest ÂForce
     Start-ScheduledTask -TaskName $TaskName
-}
-
-function Stop-Razer {
-    Get-Process "Razer*" | Stop-Process -Force
 }
 
 function Test-Connections {
@@ -1979,43 +1975,6 @@ function Test-TcpPort {
         'State'    = $State
     }
     return $TcpPort
-}
-
-function Update-AdDescriptionWithLastLogon {
-    
-}
-
-function Update-GitHubRepo {
-    param(
-        [string]$Author,
-        [string]$Repo,
-        [string]$Branch,
-        [string]$Path
-    )
-    $RepoToUpdate = "https://github.com/$Author/$Repo"
-    $Response = Invoke-WebRequest -Uri "$RepoToUpdate/commits"
-    if ($Response.StatusCode -eq '200') {
-        $LastCommit = ($Response.Links.href | Where-Object { $_ -like "/$Author/$Repo/commit/*" } | Select-Object -First 1).Split("/")[4].Substring(0,7)
-        $Git = "$Path\.git\"
-        $FETCH_HEAD = "$Git\FETCH_HEAD"
-        $LastCommitDownloaded = $null
-        if ((Test-Path -Path $Path) -and (Test-Path -Path $Git)) {
-            $LastCommitDownloaded = (Get-Content -Path $FETCH_HEAD).SubString(0,7)
-        }
-        if ($LastCommitDownloaded -ne $LastCommit) {
-            Write-Output "[!] Updating the local branch of $Repo."
-            Invoke-WebRequest -Uri "$RepoToUpdate/archive/refs/heads/$Branch.zip" -OutFile "$Repo.zip"
-            Expand-Archive -Path "$Repo.zip"
-            Move-Item -Path "$Repo\$Repo-$Branch" -Destination $Path
-            New-Item -Path $FETCH_HEAD -Force | Out-Null
-            (Get-Item -Path $Git).Attributes += "Hidden"
-            Add-Content -Path $FETCH_HEAD -Value $LastCommit -Force
-            Remove-Item -Path "$Repo.zip"
-            Remove-Item -Path "$Repo" -Recurse
-        } else {
-            Write-Output "[+] Nothing to update for the local branch of $Repo."
-        }
-    }
 }
 
 function Unblock-TrafficToIpAddress {
