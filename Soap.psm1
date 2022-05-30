3c5856e4d9922f64211b74ddf49a2eeaf1125d08
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2022-05-29 21:55:50 -0700

    Update Soap.psm1

--- Soap.psm1
+++ Soap.psm1
@@ -480,6 +480,21 @@ function Get-DnsEvents {
     Format-Table -AutoSize
 }
 
+function Get-DscResourcesRequired {
+    Param([string[]]$Resources = @("AuditPolicyDsc","xBitLocker","NetworkingDsc"))
+    $DownloadStartTime = Get-Date
+    $OutputFile = "DscResources.zip"
+    Install-Module -Name $Resources -Scope CurrentUser -Force
+    if (Test-Path $OutputFile) { Remove-Item $OutputFile -Force }
+    $env:PSModulePath -split ';' | 
+    Where-Object { $_ -like "*$env:USERNAME*" } |
+    Get-ChildItem | 
+    Where-Object { $_.LastWriteTime -gt $DownloadStartTime } |
+    Select-Object -ExpandProperty FullName |
+    Compress-Archive -DestinationPath "DscResources.zip"
+}
+
+
 function Get-GitHubRepo {
     <#
     .SYNOPSIS
