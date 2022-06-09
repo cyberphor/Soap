52c82104d611c9b341b2aac7aba268d8c9d71ac7
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2022-06-08 21:40:32 -0700

    Update Soap.psm1

--- Soap.psm1
+++ Soap.psm1
@@ -866,6 +866,32 @@ function Get-LogonEvents {
     }
 }
 
+function Get-PowerShellEvents {
+    Param(
+        [string]$Whitelist,
+        [switch]$Verbose    
+    )
+    if ($Whitelist) {
+        $Exclusions = Get-Content $Whitelist -ErrorAction Stop
+    }
+    $FilterHashTable = @{
+        LogName = "Microsoft-Windows-PowerShell/Operational"
+        Id = 4104
+    }
+    if ($Verbose) {
+        Get-WinEvent -FilterHashtable $FilterHashTable |
+        Read-WinEvent |
+        Where-Object { $_.ScriptBlockText -notin $Exclusions }
+    } else {
+        Get-WinEvent -FilterHashtable $FilterHashTable |
+        Read-WinEvent  | 
+        Where-Object { $_.ScriptBlockText -notin $Exclusions } |
+        Group-Object -Property ScriptBlockText -NoElement |
+        Sort-Object -Property Count -Descending |
+        Format-Table -AutoSize
+    }
+}
+
 function Get-ProcessCreationEvents {
     Param(
         [string]$Whitelist,
