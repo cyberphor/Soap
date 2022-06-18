620802762cfb86a5f3cfe178daff5d85eddb69ab
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2022-06-17 21:47:38 -0700

    Update Soap.psm1

--- Soap.psm1
+++ Soap.psm1
@@ -902,6 +902,28 @@ function Get-ProcessCreationEvents {
     }
 }
 
+function Get-ProcessNetConnection {
+    $NetworkConnections = Get-NetTCPConnection -State Established
+    Get-Process |
+    ForEach-Object {
+        $OwningProcess = $_.Id
+        $OwningProcessName = $_.ProcessName
+        $OwningProcessPath = $_.Path
+        $NetworkConnections |
+        Where-Object {
+            $_.LocalAddress -ne "::1" -and
+            $_.LocalAddress -ne "127.0.0.1" -and
+            $_.OwningProcess -eq $OwningProcess
+        } | Select-Object `
+            @{ Name = "ProcessId"; Expression = {$_.OwningProcess} },`
+            @{ Name = "ProcessName"; Expression = {$OwningProcessName} },`
+            LocalAddress,LocalPort,RemoteAddress,RemotePort,`
+            @{ Name = "Path"; Expression = {$OwningProcessPath} }`
+    } | 
+    Sort-Object -Property ProcessId | 
+    Format-Table -AutoSize
+}
+
 function Get-ProcessCreationReport {
     <#
         .SYNOPSIS
