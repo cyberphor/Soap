50127ba32c224dcdfe94fe006f26d7a5f31eda6f
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2022-06-21 20:45:44 -0700

    Update Soap.psm1

--- Soap.psm1
+++ Soap.psm1
@@ -65,12 +65,57 @@ function Clear-AuditPolicy {
 }
 
 function ConvertFrom-Base64 {
-    param([Parameter(Mandatory, ValueFromPipeline)]$String)
+    <#
+        .SYNOPSIS
+        Decodes Base64 strings. 
+
+        .DESCRIPTION
+        Decodes Base64 string objects into UTF-16 Little Endian objects.
+
+        .INPUTS
+        This function accepts piped objects.
+
+        .OUTPUTS
+        System.String.
+
+        .EXAMPLE
+        PS> "dABlAHMAdAAtAGMAbwBuAG4AZQBjAHQAaQBvAG4AIAA4AC4AOAAuADgALgA4AA==" | ConvertFrom-Base64
+        test-connection 8.8.8.8
+
+        .LINK
+        https://github.com/cyberphor/Soap
+    #>
+    Param([Parameter(Mandatory, ValueFromPipeline)]$String)
     [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($String))
 }
 
 function ConvertTo-Base64 {
-    param([Parameter(Mandatory, ValueFromPipeline)]$String)
+    <#
+        .SYNOPSIS
+        Encodes objects into Base64 strings. 
+
+        .DESCRIPTION
+        Encodes UTF-16 Little Endian objects into Base64 string objects.
+
+        .INPUTS
+        This function accepts piped objects.
+
+        .OUTPUTS
+        System.String.
+
+        .EXAMPLE
+        PS> echo "test-connection 8.8.8.8" | ConvertTo-Base64
+        dABlAHMAdAAtAGMAbwBuAG4AZQBjAHQAaQBvAG4AIAA4AC4AOAAuADgALgA4AA==
+        
+        PS> powershell -e dABlAHMAdAAtAGMAbwBuAG4AZQBjAHQAaQBvAG4AIAA4AC4AOAAuADgALgA4AA==
+        Source        Destination     IPV4Address      IPV6Address                              Bytes    Time(ms) 
+        ------        -----------     -----------      -----------                              -----    -------- 
+        LAPTOP-H4T... 8.8.8.8         8.8.4.4                                                   32       18       
+        LAPTOP-H4T... 8.8.8.8         8.8.4.4                                                   32       22       
+        LAPTOP-H4T... 8.8.8.8         8.8.4.4                                                   32       22       
+        LAPTOP-H4T... 8.8.8.8         8.8.4.4                                                   32       17       
+    #>
+    Param([Parameter(Mandatory, ValueFromPipeline)]$String)
     $Bytes = [System.Text.Encoding]::Unicode.GetBytes($String)
     [Convert]::ToBase64String($Bytes)
 }
