332fb502e12dc5ef48962ce402bc4dec4071c442
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2022-07-05 10:25:30 -0700

    Update Soap.psm1

--- Soap.psm1
+++ Soap.psm1
@@ -176,14 +176,71 @@ function ConvertTo-IpAddress {
 }
 
 function Disable-Firewall {
+    <#
+        .SYNOPSIS
+        Disables the firewall. 
+
+        .DESCRIPTION
+        Disables the domain, public, and private firewall profile. 
+
+        .INPUTS
+        None. This function does not accept piped objects.
+
+        .OUTPUTS
+        None.
+
+        .EXAMPLE
+        PS> Disable-Firewall
+
+        .LINK
+        https://github.com/cyberphor/Soap
+    #>
     Set-NetFirewallProfile -Name domain,public,private -Enabled False
 } 
 
 function Disable-Ipv6 {
+    <#
+        .SYNOPSIS
+        Disables IPv6. 
+
+        .DESCRIPTION
+        Disables IPv6 binding on all network adapters. 
+
+        .INPUTS
+        None. This function does not accept piped objects.
+
+        .OUTPUTS
+        None.
+
+        .EXAMPLE
+        PS> Disable-Firewall
+
+        .LINK
+        https://github.com/cyberphor/Soap
+    #>
     Disable-NetAdapterBinding -Name "*" -ComponentID ms_tcpip6
 }
 
 function Edit-Module {
+    <#
+        .SYNOPSIS
+        Opens the specified PowerShell module using PowerShell ISE. 
+
+        .DESCRIPTION
+        Opens the specified PowerShell script module file (.psm1) using PowerShell ISE. 
+
+        .INPUTS
+        None. This function does not accept piped objects.
+
+        .OUTPUTS
+        None.
+
+        .EXAMPLE
+        PS> Edit-Module "soap"
+
+        .LINK
+        https://github.com/cyberphor/Soap
+    #>
     Param([Parameter(Mandatory)][string]$Name)
     $Module = Get-Module | Where-Object { $_.Path -like "*$Name.psm1" }
     if ($Module) { 
@@ -194,10 +251,48 @@ function Edit-Module {
 }
 
 function Enable-Firewall {
+    <#
+        .SYNOPSIS
+        Enables the firewall. 
+
+        .DESCRIPTION
+        Enables the domain, public, and private firewall profile. 
+
+        .INPUTS
+        None. This function does not accept piped objects.
+
+        .OUTPUTS
+        None.
+
+        .EXAMPLE
+        PS> Enable-Firewall
+
+        .LINK
+        https://github.com/cyberphor/Soap
+    #>
     Set-NetFirewallProfile -Name domain,public,private -Enabled true
 }
 
 function Enable-Ipv6 {
+    <#
+        .SYNOPSIS
+        Enables IPv6. 
+
+        .DESCRIPTION
+        Enables IPv6 binding for on network adapters. 
+
+        .INPUTS
+        None. This function does not accept piped objects.
+
+        .OUTPUTS
+        None.
+
+        .EXAMPLE
+        PS> Disable-Firewall
+
+        .LINK
+        https://github.com/cyberphor/Soap
+    #>
     Enable-NetAdapterBinding -Name "*" -ComponentID ms_tcpip6
 }
 
