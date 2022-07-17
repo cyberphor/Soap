c94b03b7999027231b00d9cc6b5d18f63fa932cf
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2022-07-16 17:21:40 -0700

    Update Soap.psm1

--- Soap.psm1
+++ Soap.psm1
@@ -1551,6 +1551,18 @@ function New-AdForest {
     Install-ADDSForest @Parameters
 }
 
+function New-Alert {
+    Param([Parameter(Mandatory, ValueFromPipeline)][string]$Message)
+    New-EventLog -LogName "Alerts" -Source "Custom" -ErrorAction SilentlyContinue
+    Write-EventLog `
+        -Category 0 `
+        -EntryType Warning `
+        -EventID 5000 `
+        -LogName "Alerts" `
+        -Message $Message `
+        -Source "Custom" 
+}
+
 function New-CustomViewsForSysmon {
     $SysmonFolder = "C:\ProgramData\Microsoft\Event Viewer\Views\Sysmon"
     if (-not (Test-Path -Path $SysmonFolder)) {
