36d10d4646c5776fcea1a4138428e5e97cbfb5b5
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2023-02-08 07:13:33 -0800

    Create demo.ps1

---
+++ demo.ps1
@@ -0,0 +1,6 @@
+﻿Import-Module .\Soap.psm1
+
+Get-WinEvent -FilterHashtable @{ Id = 4625; Logname = "Security" } | 
+Read-WinEvent | 
+Where-Object { $_.LogonType -eq 2 } | 
+Select-Object -First 10 -Property TimeCreated, EventRecordId, TargetUserName, IpAddress
